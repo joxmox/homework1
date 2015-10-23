a9b3cb75782d1273de72031a5cb5c8dfeb311d85
Stefan Wramner <stefan.wramner@seb.se>
2015-10-23 10:27:25 +0200

    Even nicer...

--- src/lib/StringUtils.cpp
+++ src/lib/StringUtils.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 namespace seb {
   namespace str {
-     string 		lowercase	(const string& s)
+     string lowercase (const string& s)
      {
          string    r;
 
@@ -24,7 +24,7 @@ namespace seb {
          return r;
      }
 
-     string 		uppercase	(const string& s)
+     string uppercase (const string& s)
      {
     	     string    r;
 
@@ -34,33 +34,33 @@ namespace seb {
     	      return r;
      }
 
-     string 		truncate	(const string& s, const int w)
+     string truncate (const string& s, const int w)
      {
     	 return s.substr(0, w);
      }
 
-     string 		left		(const string& s, const int w, const char p)
+     string left (const string& s, const int w, const char p)
      {
     	 return (s.length() >= w) ? truncate(s, w) : s + string(w-s.length(), p);
 
      }
 
-     string 		right		(const string& s, const int w, const char p)
+     string right (const string& s, const int w, const char p)
      {
     	 return (s.length() >= w) ? s.substr(s.length()-w, s.length()) : string(w-s.length(), p) + s;
      }
 
-     string			pad_around_string(const string& s, const int w, const char p)
+     string pad_around_string (const string& s, const int w, const char p)
      {
     	 return (string((w-s.length()+1)/2, 'P') + s + string((w-s.length())/2, 'P'));
      }
 
-     string 		center		(const string& s, const int w, const char p)
+     string center (const string& s, const int w, const char p)
      {
     	 return (s.length() >= w) ? s.substr((s.length()-w)/2,w) : pad_around_string(s, w, p);
      }
 
-     vector<string> split		(const string& s, const char delim)
+     vector<string> split (const string& s, const char delim)
 	{
     	 vector<string> internal;
     	 stringstream tmp(s);
@@ -72,7 +72,7 @@ namespace seb {
     	 return internal;
     }
 
-     string 		join		(const vector<string>& v, const char sep)
+     string join (const vector<string>& v, const char sep)
      {
     	 string tmp = "";
 
