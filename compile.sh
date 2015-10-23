74de28eef060c8421f959041050ac97370cf5acf
Karl Engström <karl.engstrom@seb.se>
2015-10-23 11:04:33 +0200

    Adding genutils to compile script + adding variables

--- compile.sh
+++ compile.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+#create directories
+mkdir -p build/{objs,bin}
+
 # how to compile a single CPP file without main()
 # g++ -std=c++14 -Wall -fmax-errors=1 -c src/lib/foobar.cpp -o build/objs/foobar.o
 
@@ -15,10 +18,19 @@ set -x
 
 
 # how to create a variable
-# CXX_FLAGS="-std=c++14 -Wall -fmax-errors=1"
+CXX_FLAGS="-std=c++14 -Wall -fmax-errors=1"
+SRCLIB="src/lib"
+SRCTST="src/tst"
+OBJS="build/objs"
+BIN="build/bin"
 
 # how to use a variable
 # g++ ${CXX_FLAGS} -c ${SRCLIB}/foobar.cpp -o ${OBJS}/foobar.o
 
 
-g++ -std=c++14 -Wall -fmax-errors=1 -c src/lib/IOUtils.cpp -o build/objs/IOUtils.o
+g++ ${CXX_FLAGS} -c ${SRCLIB}/IOUtils.cpp -o ${OBJS}/IOUtils.o
+
+g++ ${CXX_FLAGS} -c ${SRCLIB}/GenUtils.cpp -o ${OBJS}/GenUtils.o
+g++ ${CXX_FLAGS} -c ${SRCTST}/GenTest.cpp -I${SRCLIB} -o ${OBJS}/GenTest.o
+
+g++ ${OBJS}/GenUtils.o ${OBJS}/GenTest.o -o ${BIN}/GenTest
