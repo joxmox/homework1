34a7fbc1118cba498acd5c873cd9b08d5e35170f
Karl Engström <karl.engstrom@seb.se>
2015-10-23 10:54:32 +0200

    Creating non-existing directories

--- compileGen.sh
+++ compileGen.sh
@@ -8,6 +8,8 @@ set -x
 # how to link a bunch of *.o files into an exe
 #g++ build/objs/foobar.o build/objs/app.o -o build/bin/app
 
+mkdir -p build/{objs,bin}
+
 g++ -std=c++14 -Wall -fmax-errors=1 -c src/lib/GenUtils.cpp -o build/objs/GenUtils.o
 g++ -std=c++14 -Wall -fmax-errors=1 -Isrc/lib -c src/tst/GenTest.cpp -o build/objs/GenTest.o
 
