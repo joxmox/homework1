2ce32153a37d620845c779c8d2c445e9a2b73967
Karl Engström <karl.engstrom@seb.se>
2015-10-23 12:42:44 +0200

    Moving all unittest build and execution to test.sh

--- compile.sh
+++ compile.sh
@@ -2,40 +2,28 @@
 set -e
 set -x
 
-#create directories
+###
+### Create directories
+###
 mkdir -p build/{objs,bin}
 
-# how to compile a single CPP file without main()
-# g++ -std=c++14 -Wall -fmax-errors=1 -c src/lib/foobar.cpp -o build/objs/foobar.o
-
-# how to link a bunch of *.o files into an exe
-# g++ build/objs/foobar.o build/objs/app.o -o build/bin/app
-
-
-# how to tell the compiler where to find include files
-# included by '#include "foobar.hpp"'
-# g++ -Isrc/lib ...
-
-
-# how to create a variable
+###
+### Variables
+###
 CXX_FLAGS="-std=c++14 -Wall -fmax-errors=1"
 SRCLIB="src/lib"
 SRCTST="src/tst"
 OBJS="build/objs"
 BIN="build/bin"
 
-# how to use a variable
-# g++ ${CXX_FLAGS} -c ${SRCLIB}/foobar.cpp -o ${OBJS}/foobar.o
-
-
+###
+### Compile
+###
 g++ ${CXX_FLAGS} -c ${SRCLIB}/IOUtils.cpp -o ${OBJS}/IOUtils.o
 g++ ${CXX_FLAGS} -c ${SRCLIB}/StringUtils.cpp -o ${OBJS}/StringUtils.o
 g++ ${CXX_FLAGS} -c ${SRCLIB}/GenUtils.cpp -o ${OBJS}/GenUtils.o
-g++ ${CXX_FLAGS} -c ${SRCTST}/StringTest.cpp -I${SRCLIB} -o ${OBJS}/StringTest.o
-g++ ${CXX_FLAGS} -c ${SRCTST}/GenTest.cpp -I${SRCLIB} -o ${OBJS}/GenTest.o
 
 ###
 ### Linking
 ###
-g++ ${OBJS}/GenUtils.o ${OBJS}/GenTest.o -o ${BIN}/GenTest
-g++ ${OBJS}/StringUtils.o ${OBJS}/StringTest.o -o ${BIN}/StringTest
+
--- test.sh
+++ test.sh
@@ -2,14 +2,36 @@
 set -e
 set -x
 
-# how to compile a single CPP file without main()
-# g++ -std=c++14 -Wall -fmax-errors=1 -c src/lib/foobar.cpp -o build/objs/foobar.o
 
-# how to link a bunch of *.o files into an exe
-# g++ build/objs/foobar.o build/objs/app.o -o build/bin/app
-
+###
+### Library compile
+###
 . ./compile.sh
 
-g++ -std=c++14 -Wall -fmax-errors=1 -Isrc/lib -c src/tst/IOUtilsUnittest.cpp -o build/objs/IOUtilsUnittest.o
-g++ build/objs/IOUtils.o build/objs/IOUtilsUnittest.o -o build/bin/IOUtilsUnittest
-build/bin/IOUtilsUnittest
+###
+### Variables
+###
+CXX_FLAGS="-std=c++14 -Wall -fmax-errors=1"
+SRCLIB="src/lib"
+SRCTST="src/tst"
+OBJS="build/objs"
+BIN="build/bin"
+
+###
+### Compile
+###
+g++ ${CXX_FLAGS} -c ${SRCTST}/IOUtilsUnittest.cpp -I${SRCLIB} -o ${OBJS}/IOUtilsUnittest.o
+g++ ${CXX_FLAGS} -c ${SRCTST}/StringTest.cpp -I${SRCLIB} -o ${OBJS}/StringTest.o
+g++ ${CXX_FLAGS} -c ${SRCTST}/GenTest.cpp -I${SRCLIB} -o ${OBJS}/GenTest.o
+
+###
+### Linking
+###
+g++ ${OBJS}/IOUtils.o ${OBJS}/IOUtilsUnittest.o -o ${BIN}/IOUtilsUnittest
+g++ ${OBJS}/GenUtils.o ${OBJS}/GenTest.o -o ${BIN}/GenTest
+g++ ${OBJS}/StringUtils.o ${OBJS}/StringTest.o -o ${BIN}/StringTest
+
+${BIN}/IOUtilsUnittest
+${BIN}/GenTest
+${BIN}/StringTest
+
