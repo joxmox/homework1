76e8fd040948440e788da1d16de4c98d3fb6bf23
Karl Engström <karl.engstrom@seb.se>
2015-11-13 10:23:02 +0100

    updated compile and test script to include homework2 files

--- compile.sh
+++ compile.sh
@@ -23,6 +23,12 @@ g++ ${CXX_FLAGS} -c ${SRCLIB}/IOUtils.cpp -o ${OBJS}/IOUtils.o
 g++ ${CXX_FLAGS} -c ${SRCLIB}/StringUtils.cpp -o ${OBJS}/StringUtils.o
 g++ ${CXX_FLAGS} -c ${SRCLIB}/GenUtils.cpp -o ${OBJS}/GenUtils.o
 
+g++ ${CXX_FLAGS} -c ${SRCLIB}/boolean.hpp -o ${OBJS}/boolean.o
+g++ ${CXX_FLAGS} -c ${SRCLIB}/integer.hpp -o ${OBJS}/integer.o
+g++ ${CXX_FLAGS} -c ${SRCLIB}/real.hpp -o ${OBJS}/real.o
+g++ ${CXX_FLAGS} -c ${SRCLIB}/text.hpp -o ${OBJS}/text.o
+g++ ${CXX_FLAGS} -c ${SRCLIB}/IndexedFile.hpp -o ${OBJS}/IndexedFile.o
+
 ###
 ### Linking
 ###
--- test.sh
+++ test.sh
@@ -24,6 +24,11 @@ g++ ${CXX_FLAGS} -c ${SRCTST}/IOUtilsUnittest.cpp -I${SRCLIB} -o ${OBJS}/IOUtils
 g++ ${CXX_FLAGS} -c ${SRCTST}/StringTest.cpp -I${SRCLIB} -o ${OBJS}/StringTest.o
 g++ ${CXX_FLAGS} -c ${SRCTST}/GenTest.cpp -I${SRCLIB} -o ${OBJS}/GenTest.o
 
+g++ ${CXX_FLAGS} -c ${SRCTST}/IndexedFile_test.cpp -I${SRCLIB} -o ${OBJS}/IndexedFile_test.o
+g++ ${CXX_FLAGS} -c ${SRCTST}/integer_test.cpp -I${SRCLIB} -o ${OBJS}/integer_test.o
+g++ ${CXX_FLAGS} -c ${SRCTST}/text_test.cpp -I${SRCLIB} -o ${OBJS}/text_test.o
+
+
 ###
 ### Linking
 ###
@@ -31,7 +36,17 @@ g++ ${OBJS}/IOUtils.o ${OBJS}/IOUtilsUnittest.o -o ${BIN}/IOUtilsUnittest
 g++ ${OBJS}/GenUtils.o ${OBJS}/GenTest.o -o ${BIN}/GenTest
 g++ ${OBJS}/StringUtils.o ${OBJS}/StringTest.o -o ${BIN}/StringTest
 
+g++ ${OBJS}/IndexedFile.o ${OBJS}/IndexedFile_test.o -o ${BIN}/IndexedFile_test
+g++ ${OBJS}/integer.o ${OBJS}/integer_test.o -o ${BIN}/integer_test
+g++ ${OBJS}/text.o ${OBJS}/text_test.o -o ${BIN}/text_test
+
+
 ${BIN}/IOUtilsUnittest
 ${BIN}/GenTest
 ${BIN}/StringTest
+
+${BIN}/IndexedFile_test
+${BIN}/integer_test
+${BIN}/text_test
+
 
