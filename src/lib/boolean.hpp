1826902dd003f1c351d99ce0db3b38946472f89d
Karl Engström <karl.engstrom@seb.se>
2015-11-16 14:34:35 +0100

    adding boolean class

--- src/lib/boolean.hpp
+++ src/lib/boolean.hpp
@@ -1,26 +1,60 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stdexcept>
+
+namespace SEB {
+	namespace datatypes {
+		using namespace std;
 
 
-class boolean {
-    char  payload;
-    constexpr static char FALSE = 'F';
-    constexpr static char TRUE  = 'T';
+		class boolean {
+			char  payload;
+			constexpr static char FALSE = 'F';
+			constexpr static char TRUE  = 'T';
 
-  public:
-    boolean() { payload = FALSE; }
-    boolean(bool v)  {}
-    
-    bool value() const {
-        return payload == TRUE;
-    }
-    void value(bool v) {
-        payload = (v ? TRUE : FALSE);
-    }
+		  public:
+			boolean() { payload = FALSE; }
+			boolean(bool v)  {
+				value(v);
+			}
 
-    operator bool() const {return value();}
-    boolean&  operator=(bool v) {value(v);}
+			bool value() const {
+				return payload == TRUE;
+			}
+			void value(bool v) {
+				payload = (v ? TRUE : FALSE);
+			}
 
-    //...
-};
+			operator bool() const {return value();}
+			boolean&  operator=(bool v) {value(v); return *this;}
 
-//...
+		};
+		inline ostream& operator<<(ostream& os, const boolean& s) {
+			return os << (s.value() ? "T": "F");
+		}
+
+		inline istream& operator>>(istream& is, boolean& s) {
+			char buf[1];
+			is >> buf;
+			s = buf[0];
+			return is;
+		}
+
+		inline bool operator==(const boolean& left, const boolean& right) {
+			bool bLeft = left;
+			bool bRight = right;
+			return bLeft == bRight;
+		}
+
+		inline bool operator<(const boolean& left, const boolean& right) {
+			bool bLeft = left;
+			bool bRight = right;
+			return bLeft < bRight;
+		}
+	}
+}
+
 
