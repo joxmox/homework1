bbe692c50d5391cb9f2a38de4929b07ae63e89fd
Karl Engström <karl.engstrom@seb.se>
2015-11-16 16:09:18 +0100

    command line arguments for accountsdbdumper

--- src/app/AccountsDbDumper.cpp
+++ src/app/AccountsDbDumper.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <fstream>
-#include <unordered_set>
 #include "IndexedFile.hpp"
-#include "StringUtils.hpp"
-#include "GenUtils.hpp"
-#include "IOUtils.hpp"
 #include "Account.hpp"
 
 using namespace std;
@@ -14,6 +9,16 @@ using namespace SEB::trans;
 int main(int argc, char* argv[])
 {
 	string filename = "accounts.db";
+	for( int idx = 1; idx< argc; ++idx)
+	{
+		string arg = argv[idx];
+		if (arg == "-f" ) {
+			filename = argv[++idx];
+		} else {
+			cerr << "usage: "<<argv[0]<<" [-f <filename>] \n";
+			return 1;
+		}
+	}
 	IndexedFile<SEB::trans::Account> db {filename, OpenMode::in};
 	for (auto  acc : db) {
 		cout << acc << endl; 
