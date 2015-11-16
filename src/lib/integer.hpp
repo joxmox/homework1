4df8a3b58186cab1669db6ab067b48657a727d4b
sebis <sebis@sebis-VirtualBox>
2015-11-16 12:39:31 +0100

    Integer and Integer_test implemented

--- src/lib/integer.hpp
+++ src/lib/integer.hpp
@@ -5,79 +5,87 @@
 #include <string>
 #include <stdexcept>
 
-namespace NNN {
-    using namespace std;
+namespace SEB {
+	namespace datatypes {
+		using namespace std;
+		template<size_t N>
+		class integer {
+			char payload[N];
 
-    template<size_t N>
-    class integer {
-        char payload[N];
+			size_t last() const {
+				size_t k = 0;
+				for (; k < N && payload[k]; ++k) { }
+				return k;
+			}
 
-        size_t last() const {
-            //...same as for text<N>
-        }
+			void pad(size_t pos) {
+				for (auto k = pos; k < N; ++k) payload[k] = '\0';
+			}
 
-        void pad(size_t pos) {
-            //...same as for text<N>
-        }
+		public:
+			constexpr size_t capacity() const { return N; }
 
-    public:
-        constexpr size_t capacity() const { return N; }
+			integer(int v) {
+				value(v);
+			}
 
-        integer(int v) {
-            //...
-        }
+			integer() {
+				value(0);
+			}
 
-        integer() {
-            //...
-        }
+			~integer() = default;
+			integer(const integer<N>&) = default;
+			integer<N>& operator=(const integer<N>&) = default;
 
-        ~integer() = default;
-        integer(const integer<N>&) = default;
-        integer<N>& operator=(const integer<N>&) = default;
+			int value() const {
+				string s{payload, payload + last()};
+				return stoi(s);
+			}
 
-        int value() const {
-            string s{payload, payload + last()};
-            return stoi(s);
-        }
+			void value(int v) {
+				string s = to_string(v);
+				if (s.size() > N) {
+					throw out_of_range("value(int): too many chars=" + s);
+				}
+				auto pos = s.copy(payload, N);
+				if (pos < N) pad(pos);
+			}
 
-        void value(int v) {
-            string s = to_string(v);
-            if (s.size() > N) {
-                throw out_of_range("value(int): too many chars=" + s);
-            }
-            auto pos = s.copy(payload, N);
-            if (pos < N) pad(pos);
-        }
+			operator int() const {
+				return value();
+			}
 
-        operator int() const { 
-            //...
-        }
+			integer<N>& operator=(int v) {
+				value(v);
+				return *this;
+			}
+		};
 
-        integer<N>& operator=(int v) {
-            //...
-            return *this;
-        }
-    };
+		template<size_t N>
+		inline ostream& operator<<(ostream& os, const integer<N>& s) {
+			return os << s.value();
+		}
 
-    template<size_t N>
-    inline ostream& operator<<(ostream& os, const integer<N>& s) {
-        //...
-    }
+		template<size_t N>
+		inline istream& operator>>(istream& is, integer<N>& s) {
+			char buf[N + 1];
+			is >> buf;
+			s = stoi(buf);
+			return is;
+		}
 
-    template<size_t N>
-    inline istream& operator>>(istream& is, integer<N>& s) {
-        //...
-        return is;
-    }
+		template<size_t N>
+		inline bool operator==(const integer<N>& left, const integer<N>& right) {
+			int iLeft = left;
+			int iRight = right;
+			return iLeft == iRight;
+		}
 
-    template<size_t N>
-    inline bool operator==(const integer<N>& left, const integer<N>& right) {
-        //...
-    }
-
-    template<size_t N>
-    inline bool operator<(const integer<N>& left, const integer<N>& right) {
-        //...
-    }
-    
+		template<size_t N>
+		inline bool operator<(const integer<N>& left, const integer<N>& right) {
+			int iLeft = left;
+			int iRight = right;
+			return iLeft < iRight;
+		}
+	}
 }
--- src/tst/integer_test.cpp
+++ src/tst/integer_test.cpp
@@ -5,7 +5,7 @@
 #include <utility>
 
 using namespace std;
-using NNN::integer;
+using SEB::datatypes::integer;
 
 void empty_should_have_value_zero() {
     integer<4> n;
@@ -19,7 +19,9 @@ void initialization_should_work() {
 }
 
 void assignment_should_work() {
-    //...
+    integer<4> n;
+    n = 5;
+    assert (n.value() == 5);
 }
 
 void overflow_should_throw() {
@@ -35,22 +37,40 @@ void overflow_should_throw() {
 }
 
 void relops_should_work() {
-    //...
+    integer<4> n1{10};
+    integer<4> n2{10};
+    assert(n1 == n2);
+    n1 = 5;
+    assert(n1 < n2);
 }
 
 void out_stream_should_work() {
-    //...
+    integer<8> t{1234};
+    ostringstream buf;
+    buf << t;
+    assert(buf.str() == "1234");
 }
 
 void in_stream_should_work() {
-    //...
+	integer<4> n;
+	assert(n.value() == 0);
+
+	istringstream buf{"1234"};
+	buf >> n;
+	assert(n.capacity() == 4);
+	assert(n.value() == 1234);
 }
 
-//...
+
 
 int main(int numArgs, char* args[]) {
     empty_should_have_value_zero();
-    //...
+    initialization_should_work();
+    assignment_should_work();
+    overflow_should_throw();
+    relops_should_work();
+    out_stream_should_work();
+    in_stream_should_work();
 
     cout << "All tests passed\n";
     return 0;
